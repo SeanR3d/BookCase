0a335c00d13bdff2254e60930681b877677a5f57
Sean <Seanr1194@gmail.com>
2019-11-26 17:24:00 -0500

    Added duration field to Book class

--- app/src/main/java/edu/temple/bookcase/Book.java
+++ app/src/main/java/edu/temple/bookcase/Book.java
@@ -9,12 +9,14 @@ public class Book implements Serializable {
     String author;      // The author of the book
     int published;      // The year the book was published
     String coverURL;    // A URL to the image representing the book cover
+    int duration;       // The length of the book in seconds
 
-    public Book (int id, String title, String author, int published, String coverURL) {
+    public Book (int id, String title, String author, int published, String coverURL, int duration) {
         this.id = id;
         this.title = title;
         this.author = author;
         this.published = published;
         this.coverURL = coverURL;
+        this.duration = duration;
     }
 }
